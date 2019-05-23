3017e1e9ba71009ae4778bd5e7462bb0ff0bb178
Chris Chen <chen08900@gmail.com>
2019-05-23 17:07:15 -0500

    remove outdated fig script

--- src/fig_scripts/fig_motif.R
+++
@@ -1,5 +0,0 @@
-library(ggplot2)
-
-mean_data <- read.csv('~/Dropbox/MU/workspace/encode_dream/data/figs_data/fig_motif/motif2.csv',header = F)
-df <- cbind.data.frame('x'=1:nrow(mean_data),'y'=mean_data$V1)
-ggplot(df,aes(x,y)) + geom_point() + geom_smooth() +xlab('Subsequence coordinate')+ylab('DNase score')
